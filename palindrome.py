95aaeec9266a9aaef46876b479917827f47c554e
DJ Knox <dhknox@gmail.com>
2019-06-04 12:43:02 -0400

    refactor palindrome checking steps into functions

--- palindrome.py
+++ palindrome.py
@@ -1,19 +1,25 @@
+# palindrome.py asks the user for a string and determines if it is palindromic
+
+# import re library for using regular expressions
 import re
 
-# ask user for one or more sentences
+def strip_whitespace_and_punctuation(phrase):
+    """remove all characters except for alphabetical letters from a string"""
+    return re.sub(r'[^A-Za-z]', '', phrase)
+
+def check_if_palindrome(phrase):
+    """return True if the string is a palindrome"""
+    # make lowercase
+    phrase = phrase.lower()
+    # strip whitespace and punctuation
+    stripped_phrase = strip_whitespace_and_punctuation(phrase)
+    #check if reverse is same as normal and return
+    return stripped_phrase == stripped_phrase[::-1]
+
+# ask user for a string
 user_input = input("Enter a phrase, a sentence, or multiple sentences to see if it is a palindrome! ")
 
-# Use "is a palindrome" and "is not a palindrome" in your output in order for the tests to pass.
-def check_if_palindrome(phrase):
-    # strip of whitespace and punctuation
-    phrase = re.sub(r'[^A-Za-z]', '', phrase)
-    #check if reverse is same as normal
-    phrase_reversed = phrase[::-1]
-    if phrase == phrase_reversed:
-        return True
-    else:
-        return False
-
+# check if palindrome and return
 if check_if_palindrome(user_input):
     print(user_input, "is a palindrome")
 else:
